69ef782862aae0572b1a1995f9bae6a973f96c07
Nawaz.Sharif <nawaz.sharif@gmail.com>
2026-06-27 20:13:24 +0530

    Added Interceptors

--- axios/axios-basics.js
+++ axios/axios-basics.js
@@ -1,52 +1,89 @@
 import axios from "axios";
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const json_placeholder_users = process.env.JSON_PLACEHOLDER_USERS
-const json_placeholder_posts = process.env.JSON_PLACEHOLDER_POSTS
+const json_placeholder_users = process.env.JSON_PLACEHOLDER_USERS;
+const json_placeholder_posts = process.env.JSON_PLACEHOLDER_POSTS;
 
-async function fetchUsers(url){
-    try{
-        const response = await axios.get(url)
-        const users = response.data;
-        console.log(users);
-        // const emails = users.map((data)=> data.email)
-        // console.log(emails ?? 'No Emails Found');
-        // console.log('status :', response.status)
-        // console.log('statusText :', response.statusText)
-        // console.log('headers :', response.headers)
-        // console.log('config :', response.config)
-        return users;
-    }
-    catch(error){
-        console.log('Error occured while fetching users :', error)
-        throw new Error('Error occured while fetching users', error)
-    }
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = "Bearer tokenabcd";
+  console.log(
+    `Request sent to ${config.url} at ${new Date().toLocaleString()}`,
+  );
+  return config;
+});
+
+axios.interceptors.response.use((response) => {
+  console.log(
+    "Response received :",
+    JSON.stringify(response.data) + "at " + new Date().toLocaleString(),
+  );
+});
+
+async function fetchUsers(url) {
+  try {
+    const response = await axios.get(url);
+    const users = response.data;
+    console.log(users);
+    // const emails = users.map((data)=> data.email)
+    // console.log(emails ?? 'No Emails Found');
+    // console.log('status :', response.status)
+    // console.log('statusText :', response.statusText)
+    // console.log('headers :', response.headers)
+    // console.log('config :', response.config)
+    return users;
+  } catch (error) {
+    console.log("Error occured while fetching users :", error);
+    throw new Error("Error occured while fetching users", error);
+  }
 }
 
 // await fetchUsers(json_placeholder_users)
 
-
-async function fetchPosts(url){
-    try{
-        const posts = await axios.get(url,{
-            headers : {
-                "Content-Type" : 'application/json',
-                "Accept" : 'application/json',
-                "Authorization" : "Bearer abcd123"
-            },
-            params : {
-                userId : 1
-            },
-            timeout : 5000
-        })
-        console.log(posts.data)
-    }
-    catch(error){
-        console.log('Error occured while fetching posts : ', error)
-        throw new Error('Internal Server Error :', error)
-    }
+async function fetchPosts(url) {
+  try {
+    const posts = await axios.get(url, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Authorization: "Bearer abcd123",
+      },
+      params: {
+        userId: 1,
+      },
+      timeout: 5000,
+    });
+    console.log(posts.data);
+  } catch (error) {
+    console.log("Error occured while fetching posts : ", error);
+    throw new Error("Internal Server Error :", error);
+  }
 }
 
-await fetchPosts(json_placeholder_posts)
+// await fetchPosts(json_placeholder_posts)
+
+async function createPost(url, data) {
+  try {
+    const response = await axios.post(url, {
+      name: "Nawaz",
+      role: "Engineer",
+    });
+    //console.log('response :', response.data);
+  } catch (error) {
+    console.error(
+      "Error occured while creating post, message :",
+      error.message,
+    );
+    console.error(
+      "Error occured while creating post, status :",
+      error.response?.status,
+    );
+    console.error(
+      "Error occured while creating post, data :",
+      error.response?.data,
+    );
+  }
+}
+
+// await createPost(json_placeholder_users);
--- axios/axios-notes.md
+++ axios/axios-notes.md
@@ -3,23 +3,23 @@
 ## 0. Axios GET syntax
 
 > axios.get(url, config)
---- 
+
+---
 
 ## 1. Sending Query Parameters
 
-Instead of writing: 
+Instead of writing:
 
 > axios.get('/users?page=1&limit=20');
-
 
 Use:
 
 ```js
-axios.get('/users', {
-    params: {
-        page: 1,
-        limit: 20
-    }
+axios.get("/users", {
+  params: {
+    page: 1,
+    limit: 20,
+  },
 });
 ```
 
@@ -34,18 +34,18 @@ axios.get('/users', {
 - Example:
 
 ```js
-await axios.get('/users', {
-    headers: {
-        Authorization: 'Bearer abc123'
-    }
+await axios.get("/users", {
+  headers: {
+    Authorization: "Bearer abc123",
+  },
 });
 ```
 
-## Sending Timeout 
+## Sending Timeout
 
 ```js
-await axios.get('/users', {
-    timeout : 5000
+await axios.get("/users", {
+  timeout: 5000,
 });
 ```
 
@@ -54,3 +54,105 @@ await axios.get('/users', {
 ## Axios POST syntax
 
 > axios.post(url, data, config)
+
+## Error Handling
+
+```js
+console.log(error.message);
+
+console.log(error.response?.status);
+
+console.log(error.response?.data);
+```
+
+## Interceptors
+
+### What is an Interceptor?
+
+> An interceptor is a function that Axios executes:
+
+- Before sending a request (Request Interceptor)
+- After receiving a response (Response Interceptor)
+
+- Think of it as middleware for Axios.
+
+```
+Your Code
+    │
+    ▼
+Request Interceptor
+    │
+    ▼
+HTTP Request
+    │
+    ▼
+Server
+    │
+    ▼
+HTTP Response
+    │
+    ▼
+Response Interceptor
+    │
+    ▼
+Your Code
+```
+
+> Why Do We Need Interceptors?
+
+Imagine your application makes 100 API calls.
+
+> Without interceptors:
+
+```js
+await axios.get(url1, {
+  headers: {
+    Authorization: token,
+  },
+});
+
+await axios.post(url2, body, {
+  headers: {
+    Authorization: token,
+  },
+});
+
+await axios.patch(url3, body, {
+  headers: {
+    Authorization: token,
+  },
+});
+```
+
+> You're repeating yourself.
+
+> Request Interceptor
+
+- A request interceptor runs before Axios sends the HTTP request.
+
+```js
+axios.interceptors.request.use((config) => {
+  console.log("Request is about to be sent");
+  return config; //Axios expects the interceptor to return the request configuration.
+});
+```
+
+> What is config?
+
+- Every Axios request is converted into a configuration object.
+
+```js
+await axios.get('/users', {
+    headers: {
+        Authorization: 'Bearer abc'
+    }
+});
+
+{
+    method: 'get',
+    url: '/users',
+    headers: {
+        Authorization: 'Bearer abc'
+    }
+}
+```
